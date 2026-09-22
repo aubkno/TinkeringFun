9c7c9c67585aaec0b5ec9030ba615b8e23de0085
Aubrey Knowles <aknowles2@zagmail.gonzaga.edu>
2026-09-22 11:33:15 -0700

    get imput from user to calculate work

---
+++ variables_lab.py
@@ -0,0 +1,9 @@
+force = float(input("Enter a force in newtons: "))
+distance = input("Enter a distance in meters: ")
+distance = float(distance)
+work = force* distance
+print("work:", work)
+time = 6
+power = work / time 
+print("work:", work)
+print(type(work))
